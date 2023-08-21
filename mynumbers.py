391e954b9ffba6e1d94066e29398d330d41229f2
Paul Kinyua <pbkinyua@gmail.com>
2023-08-21 19:44:22 +0300

    second commit

---
+++ mynumbers.py
@@ -0,0 +1,28 @@
+# +, -, /, *, **
+#add
+add = 2+2
+print(add)
+
+#subtract
+sub = 5-2
+print(sub)
+
+#divide
+divi=10/2
+print(divi)
+
+#multiply
+mult = 2*2
+print(mult)
+
+#exponential (power)
+expon = 3**4 # 3 ^ 4
+print(expon )
+
+#modulo (remainder of a division)
+mod = 5%2
+print(mod)
+
+#floor operation, divison that results into whole numbers
+floor = 30//7
+print(floor)
