dc2cc7501e948cec82da1a07787e323fe03e35cb
Paul Kinyua <pbkinyua@gmail.com>
2023-08-21 18:36:55 +0300

    first commit

---
+++ areaofcircle.py
@@ -0,0 +1,5 @@
+# area = pir^2
+r=float(input("Enter the radius: "))
+pi=22/7
+area=pi * (r**2)
+print(area)
---
+++ stringtask.py
@@ -0,0 +1,19 @@
+#first question
+name=input("Enter name: ")
+print(name.lower())
+
+#second question
+sentence_one = "The Dog Breed is German Shepherd"
+sentence_two = "Defeats for the Clinton forces, this was her moment of triumph"
+print(sentence_one[8:23])
+print(sentence_two[16:30])
+
+#third question
+split_string = "The lazy dog; ran so fast; it hit the wall."
+x = split_string.split()
+print(len(x))
+
+#fourth question
+special_string = input("Enter name: ") 
+test_str = ''.join(letter for letter in special_string if letter.isalnum())
+print(test_str)
