556322296fd39eb62728bc330c210e3ae2f24adc
Paul Kinyua <pbkinyua@gmail.com>
2023-08-29 10:18:00 +0300

    if statement tasks

--- ifstatement.py
+++ ifstatement.py
@@ -12,8 +12,21 @@ elif(num3>num1 and num3>num2) :
 
 # write a program that checks if a number is divisible by 7
 num =int(input("Enter number: "))
+mod = num % 7
+if mod == "" or mod == 0:
+    print("Divisible by 7")
+else:
+    print("Not divisible by 7")
 
 #write a program that calculates electricity bill based on the folowing:
 #First 50 units     Kshs.20 
 #Next 50 units       Kshs.40
 #After 100 units   Kshs.100
+
+num1 = float(input("Enter Units: "))
+if(num1 <= 50):
+    print("KSH. 20")
+elif(num1>50 and num1<=100):
+    print("KSH. 40")
+else:
+    print("KSH. 100")
