d2f90c1841391b9b37134d0e519580df6ddf80f0
Paul Kinyua <pbkinyua@gmail.com>
2023-09-04 20:32:16 +0300

    my functions class

---
+++ myfunction.py
@@ -0,0 +1,19 @@
+def calculator():
+    x = 7
+    y = 10
+    print (x + y)
+
+def calc():
+    x = 7
+    y = 11
+    return (x + y)
+
+def cal(num1, num2):
+    x = num1+num2
+    return(x)
+
+calculator()
+a = calc()
+b = cal(4, 5)
+print(a)
+print(b)
