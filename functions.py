172d5b86a73fd76635abd9bafeb965ee57ae581b
Paul Kinyua <pbkinyua@gmail.com>
2023-09-07 20:55:07 +0300

    import classes from one page to another

---
+++ functions.py
@@ -0,0 +1,9 @@
+def calculator_add(x,y):
+    t=x+y
+    return t
+
+def calculator_minus(x,y):
+    t=x-y
+    return t
+
+name = 'John Doe'
---
+++ runner.py
@@ -0,0 +1,9 @@
+from functions import * #import everything from the functions file
+
+t1 = calculator_add(2,5)
+print(t1)
+
+t2 = calculator_minus(6,1)
+print(t2)
+
+print(name)
